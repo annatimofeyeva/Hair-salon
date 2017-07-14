45f698fc3a24176c57b996c3856b23da7450cb32
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-14 16:52:31 -0700

    add UI routes

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.List;
+import java.util.Random;
+import org.sql2o.*;
+import java.util.UUID;
+
+public class App{
+
+public static void main(String[] args){
+  staticFileLocation("/public");
+  String layout = "templates/layout.vtl";
+
+  get("/", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+ }
+}
