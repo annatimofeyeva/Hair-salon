f8e8198b09c0aa81bd74305a900bc0a2b81de81f
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-15 09:22:39 -0700

    contancts.vtl update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,5 +19,15 @@ public static void main(String[] args){
     model.put("template", "templates/index.vtl");
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
+
+
+  get("/contacts", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/contacts.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
  }
 }
