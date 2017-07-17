231526c4ea7aa474693250d8896a18ad9e85ccf7
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-16 21:02:04 -0700

    app updated, templates added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,6 +16,7 @@ public static void main(String[] args){
 
   get("/", (request, response) -> {
     Map<String, Object> model = new HashMap<String, Object>();
+    model.put("stylists", Stylist.all());
     model.put("template", "templates/index.vtl");
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
@@ -27,10 +28,27 @@ public static void main(String[] args){
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/stylists", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/stylists.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      post("/stylists", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        String name = request.queryParams("name");
+        String image = request.queryParams("image");
+        Stylist newStylist = new Stylist(name, image);
+        newStylist.save();
+        model.put("template", "templates/stylist-success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+
+
     post("/messages", (request, response) -> {
         Map<String, Object> model = new HashMap<String, Object>();
         String name = request.queryParams("name");
-        //Team newTeam = new Team(name);
         model.put("template", "templates/messages.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
